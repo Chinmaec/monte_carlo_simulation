3c03d5f11743872d5434545d3f0b7c124f56acf7
Chinmae <ccchinmae@gmail.com>
2026-04-13 12:23:12 +0530

    added descriptions for easier application

--- test.py
+++ test.py
@@ -2,20 +2,21 @@ from mc_pricer.mc_euro import mc_european, black_scholes_price
 from mc_pricer.mc_asian import mc_asian
 from mc_pricer.mc_greeks import mc_delta, mc_gamma
 
-S0, K, T, r, sigma = 100, 100, 1.0, 0.05, 0.20
+S0, K, T, r, sigma = 100, 100, 1.0, 0.05, 0.2
+#(S0:spot price, K:strike price, T:time(days), r:rate of interest, sigma: implied volatiliy)
 option_type = "call"
 
 
 # European demo
 bs = black_scholes_price(S0, K, T, r, sigma, option_type)
-price, se, ci = mc_european(S0, K, T, r, sigma, 200_000, option_type, seed=42, method= "antithetic")
+price, se, ci = mc_european(S0, K, T, r, sigma, 500_000, option_type, seed=42, method= "antithetic")
 
 d_eur, d_eur_se, d_eur_ci = mc_delta(mc_european, S0, K, T, r, sigma, option_type=option_type, n_samples=200_000, n_rep=20)
 g_eur, g_eur_se, g_eur_ci = mc_gamma(mc_european, S0, K, T, r, sigma, option_type=option_type, n_samples=200_000, seed=42, n_rep=20)
 
 print(f"BS price: {bs:.2f}")
-print(f"European MC: {price:.2f}")
-print(f"SE: {se:.2f}")
+print(f"European MC price: {price:.2f}")
+print(f"Standard Error: {se:.2f}")
 print(f"CI95: ({ci[0]:.2f}, {ci[1]:.2f})")
 print(f"European Delta MC: {d_eur:.2f} | SE: {d_eur_se:.2f} | CI: {d_eur_ci[0]:.2f}, {d_eur_ci[1]:.2f}")
 print(f"European Gamma MC: {g_eur:.2f} | SE: {g_eur_se:.2f} | CI: {g_eur_ci[0]:.2f},{g_eur_ci[1]:.2f}")
@@ -39,8 +40,8 @@ d_as, d_as_se, d_as_ci = mc_delta(mc_asian, S0, K, T, r, sigma, bump_rel=1e-2, o
 g_as, g_as_se, g_as_ci = mc_gamma(mc_asian, S0, K, T, r, sigma, bump_rel=1e-2, option_type=option_type, 
                                   n_steps=252, n_samples=200_000, seed=42, n_rep=20)
 
-print(f"Asian MC: {price:.2f}")
-print(f"SE: {se:.2f}")
+print(f"Asian MC price: {price:.2f}")
+print(f"Standard Error: {se:.2f}")
 print(f"CI95: ({ci[0]:.2f}, {ci[1]:.2f})")
 print(f"Asian Delta MC: {d_as:.2f} | SE: {d_as_se:.2f} | CI: {d_as_ci[0]:.2f}, {d_as_ci[1]:.2f}")
 print(f"Asian Gamma MC: {g_as:.2f} | SE: {g_as_se:.2f} | CI: {g_as_ci[0]:.2f},{g_as_ci[1]:.2f}")
